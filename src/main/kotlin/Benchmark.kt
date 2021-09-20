877ff22ec5eeebfefaf64a8a7581573129ff9294
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-09-20 19:28:49 +0430

    Add the coroutines approach

--- src/main/kotlin/Benchmark.kt
+++ src/main/kotlin/Benchmark.kt
@@ -1,3 +1,6 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 
 const val ARRAY_SIZE = 2_000_000
@@ -10,11 +13,30 @@ fun main() {
         "\u001b[36mCalculating The intersection of two arrays of size $ARRAY_SIZE," +
                 " filled with random integers in: [1,20000000]...\u001b[0m"
     )
-    printMeasurementsFor {
+
+    printMeasurementsFor("Using the 'GOOD' algorithm (the desired situation): ", printResult = false) {
         with(arrays.first.toHashSet() to arrays.second.toHashSet()) {
-            println(first.filter { second.contains(it) })
+            first.filter { second.contains(it) }
         }
     }
+
+    printMeasurementsFor("Using the 'EVEN BETTER' algorithm (with the power of coroutines): ", printResult = false) {
+        with(arrays.first to arrays.second.toHashSet()) {
+            runBlocking {
+                val chunks = 50
+                val chunkSize = first.size / chunks
+                repeat(chunks) { iteration ->
+                    launch(Dispatchers.Default) {
+                        for (i in chunkSize * (iteration) until chunkSize * (iteration + 1)) {
+                            second.contains(first[i])
+                        }
+                    }.join()
+                }
+            }
+        }
+    }
+
+    printBenchmarkDetails()
 }
 
 fun generateArray() = IntArray(ARRAY_SIZE) { Random.nextInt(1, 20_000_001) }
